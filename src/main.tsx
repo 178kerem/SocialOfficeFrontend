a112abb626c66e5ad8ccb5c4745a2e66c2ff01bc
Kayra Bayer <115025648+KayraBayer@users.noreply.github.com>
2025-08-20 09:24:43 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -13,20 +13,3 @@ createRoot(document.getElementById('root')!).render(
   </StrictMode>,
 )
 
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App'; // App => Register sayfasýný render ediyor
-import Navbar from './components/navbar';
-
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <div className="min-h-screen flex flex-col bg-slate-50">
-            
-            <main className="flex-1">
-                <App />
-            </main>
-        </div>
-    </StrictMode>
-);
-
