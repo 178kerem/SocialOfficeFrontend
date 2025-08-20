9917f9ac6029ea421e0cb97d42130ecd33d6db69
Okan Kerem ÇITAK <okan.citak@tbtk.gov.tr>
2025-08-20 09:04:51 +0300

    Kardeşim Şu srcyi bi düzelt ya register ekledim

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import Navbar from './components/navbar.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App'; // App => Register sayfasýný render ediyor
+import Navbar from './components/navbar';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Navbar />
-  </StrictMode>,
-)
+    <StrictMode>
+        <div className="min-h-screen flex flex-col bg-slate-50">
+            
+            <main className="flex-1">
+                <App />
+            </main>
+        </div>
+    </StrictMode>
+);
